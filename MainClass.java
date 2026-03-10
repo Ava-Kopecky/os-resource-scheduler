23ab73f2cb3539ae5344f8d05abd405e9d23f983
Ava-Kopecky <kopeckya@uci.edu>
2026-03-10 00:07:36 +0000

    Implemented ResourceManager class

--- MainClass.java
+++ MainClass.java
@@ -134,6 +134,35 @@ class DirectoryManager
 
 class ResourceManager
 {
+    boolean[] isFree;
+
+    ResourceManager(int numberOfItems) {
+        isFree = new boolean[numberOfItems];
+        for (int i = 0; i < isFree.length; ++i)
+            isFree[i] = true;
+    }
+
+    synchronized int request() {
+        while (true) {
+            for (int i = 0; i < isFree.length; ++i) {
+                if (isFree[i]) {
+                    isFree[i] = false;
+                    return i;
+                }
+            }
+
+            try {
+                wait();   // block until someone releases resource
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    synchronized void release(int index) {
+        isFree[index] = true;
+        notify();  // let a blocked thread run
+    }
 }
 
 class DiskManager
